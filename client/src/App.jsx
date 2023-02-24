4769948c16613c7b8524c86761148f9ec167dfc6
shkor.danila@gmail.com <shkor.danila@gmail.com>
2023-02-24 21:50:45 +0300

    Проект

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (  
+    <div className="App">
+        <div class='main-grid'>
+            <div class='left-navbar-area'>
+              <nav class='left-navbar'>
+                  <ul>
+                    <li></li>
+                  </ul>
+              </nav>
+            </div>
+            <div class='account-bar-area'></div>
+            <div class='music-area'></div>
+            <div class='player-area'></div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
